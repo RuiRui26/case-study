32c069d8214fb253529fe9458762c99989ecfe88
RuiRui26 <161122914+RuiRui26@users.noreply.github.com>
2024-12-08 18:00:43 +0800

    edited offices.php

--- offices.php
+++ offices.php
@@ -1,15 +1,19 @@
 <?php
 include 'db_connection.php';
 
-$sql = "SELECT Address FROM office WHERE City = 'Glasgow'";
+$sql = "SELECT Name, Address FROM office WHERE City = 'Glasgow'";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
-    echo "<ul>";
+    
+    echo "<table border='1'>";
+    echo "<tr><th>Office Name</th><th>Address</th></tr>";  
+
+    
     while ($row = $result->fetch_assoc()) {
-        echo "<li>" . $row['Address'] . "</li>";
+        echo "<tr><td>" . $row['Name'] . "</td><td>" . $row['Address'] . "</td></tr>";
     }
-    echo "</ul>";
+
+    echo "</table>";
 } else {
     echo "No offices found in Glasgow.";
 }
