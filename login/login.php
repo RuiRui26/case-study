33c18a519bb11bc192884d32f77a641118ae00e2
noex08 <mikylam.escudero@gmail.com>
2024-12-11 14:25:06 +0800

    login
    hnde pa tapos

--- login/login.php
+++ login/login.php
@@ -1,61 +1,47 @@
 <?php
+    include '../db_connection.php';
 
+    function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+
+        return $data;
+    }
+
+    if(isset($_POST['first_name'])&&isset($_POST['last_name'])&&isset($_POST['password'])){
+
+        $first_name = validate($_POST['first_name']);
+        $last_name = validate($_POST['last_name']);
+        $pass = validate($_POST['password']);
+
+        if (empty($first_name) || empty($last_name) || empty($pass)){
+            header('location: loginPage.php?error=fill in necessary fields.');
+            exit();
+        }else{
+            $sql = "SELECT * from client where First_Name = ?";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "s", $first_name);
+            mysqli_stmt_execute($stmt);
+
+            $result = mysqli_stmt_get_result($stmt);
+            if(mysqli_num_rows($result) === 1){
+                $row = mysqli_fetch_assoc($result);
+                if(password_verify($pass, $row['Password'])){
+                    session_start();
+                    $_SESSION['Client_ID']=$row['Client_ID'];
+                    header('location: ../views/clientView/clientView.php');
+                    exit();
+                }else{
+                    header('location: case-study/login/loginPage.php?error=Incorrect password!');
+                    exit();
+                }
+            }else{
+                header('location: case-study/login/loginPage.php?error=User not found.');
+                exit();
+            }
+        }
+    }else{
+        header('location: case-study/login/loginPage.php');
+    }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CLIENT LOGIN</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-        body{
-            background-color: aliceblue;
-            padding: 10%;
-            margin: 0;
-        }
-
-        .container-md{
-            background-color: white;
-            box-shadow: 0 0 20px -15px black;
-            border-radius: 10px;
-            padding: 5%;
-        }
-    </style>
-
-</head>
-<body>
-    <div class="container-md">
-        <form action="" method="post">
-            <h1 class="text-center">Welcome to EasyDrive School</h1>
-            <p class="text-center">Are you Staff? <a class="link-primary link-opacity-50-hover link-underline link-underline-opacity-0" href="">Go here</a>.</p>
-            <div class="mb-3">
-                <label for="user_id" class="form-label">First Name</label>
-                <input class="form-control form-control-lg" type="text" name="first_name" id="first_name" placeholder="First Name">
-            </div>
-
-            <div class="mb-3">
-                <label for="user_id" class="form-label">Last Name</label>
-                <input class="form-control form-control-lg" type="text" name="last_name" id="last_name" placeholder="Last Name">
-            </div>
-            
-            <div class="mb-3">
-                <label for="user_id" class="form-label">Password</label>
-                <input input class="form-control form-control-lg" type="text" name="password" id="password" placeholder="Password">
-            </div>
-
-            <div class="d-grid gap-2">
-                <button name="submit" type="button" class="btn btn-dark btn-lg">Submit</button>
-            </div>
-            
-            <div class="form-text">
-                <span style="font-size: 18px;">Not registered yet? <a class="link-primary link-opacity-50-hover link-underline link-underline-opacity-0" href="/case-study/clientRegistration/clientRegistration.php">Register now!</a></span>
-            </div>
-            
-
-        </form>
-    </div>
-    
-</body>
-</html>
---
+++ login/loginPage.php
@@ -0,0 +1,64 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CLIENT LOGIN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body{
+            background-color: aliceblue;
+            padding: 10%;
+            margin: 0;
+        }
+
+        .container-md{
+            background-color: white;
+            box-shadow: 0 0 20px -15px black;
+            border-radius: 10px;
+            padding: 5%;
+        }
+    </style>
+
+</head>
+<body>
+    <div class="container-md">
+        <form action="login.php" method="POST">
+            <?php if(isset($_GET['error'])){ ?>
+                <p class="error"><?php echo $_GET['error']?></p>
+                <?php }?>
+            <h1 class="text-center">Welcome to EasyDrive School</h1>
+            <p class="text-center">Are you Staff? <a class="link-primary link-opacity-50-hover link-underline link-underline-opacity-0" href="">Go here</a>.</p>
+            <div class="mb-3">
+                <label for="first_name" class="form-label">First Name</label>
+                <input class="form-control form-control-lg" type="text" name="first_name" id="first_name" placeholder="First Name">
+            </div>
+
+            <div class="mb-3"> 
+                <label for="last_name" class="form-label">Last Name</label>
+                <input class="form-control form-control-lg" type="text" name="last_name" id="last_name" placeholder="Last Name">
+            </div>
+            
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input input class="form-control form-control-lg" type="password" name="password" id="password" placeholder="Password">
+            </div>
+
+            <div class="d-grid gap-2">
+                <button name="submit" type="button" class="btn btn-dark btn-lg">Submit</button>
+            </div>
+            
+            <div class="form-text">
+                <span style="font-size: 18px;">Not registered yet? <a class="link-primary link-opacity-50-hover link-underline link-underline-opacity-0" href="/case-study/clientRegistration/clientRegistration.php">Register now!</a></span>
+            </div>
+            
+
+        </form>
+    </div>
+    
+</body>
+</html>
