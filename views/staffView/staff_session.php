747f93464c708172380bae64882fde123188aed7
noex08 <mikylam.escudero@gmail.com>
2024-12-16 03:34:27 +0800

    dsad

--- views/staffView/staff_session.php
+++ views/staffView/staff_session.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] != 'staff') {
-    header("Location: login.php");
+    header("Location: ../../login-register-interview2/login.php");
     exit();
 }
 ?>
