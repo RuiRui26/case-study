f6d77a5ec4e0be36ab5225fc37dc7819f04f4852
noex08 <mikylam.escudero@gmail.com>
2024-12-20 14:47:26 +0800

    dfg

--- views/staffView/header_staff.php
+++ views/staffView/header_staff.php
@@ -29,7 +29,7 @@
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item"><a class="nav-link" href="staff_dashboard.php">Dashboard</a></li>
-                    <li class="nav-item"><a class="nav-link" href="../../add_interview_details.php">Interview</a></li> 
+                    <li class="nav-item"><a class="nav-link" href="add_interview_details.php">Interview</a></li> 
                 </ul>
                 <a class="nav-link" href="../../login-register-interview2/logout.php" style="padding: 0 20px;">Logout</a>
             </div>
